f2fcc9a7ccb7339d93fc248e8020782499c75c51
Ricky <ricky.yang.qh@gmail.com>
2026-09-02 06:52:24 +0800

    Initial Commit

---
+++ Gambling Simulator/Gambling_SimulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gambling_SimulatorApp.swift
+//  Gambling Simulator
+//
+//  Created by Ricky   Yang on 2/9/26.
+//
+
+import SwiftUI
+
+@main
+struct Gambling_SimulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
